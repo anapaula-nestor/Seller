b7399035a87cf55a36002a125b1a9ad9f04f3ee9
olgaleao <olgaleao@gmail.com>
2021-02-10 01:19:41 -0400

    created tests for sellers model

--- src/tests/model/test_seller.py
+++ src/tests/model/test_seller.py
@@ -4,7 +4,7 @@ from src.model.seller import Seller
 from src.model.base_model import BaseModel
 
 
-@pytest.mark.parametrize('active', 'id_person', [(True, 1), (False, 1)])
+@pytest.mark.parametrize('active, id_person', [(True, 1), (False, 1)])
 def test_instance(active: bool, id_person: int):
     seller = Seller(active, id_person)
     assert isinstance(seller, Seller)
@@ -13,8 +13,8 @@ def test_instance(active: bool, id_person: int):
     assert seller.id_person is id_person
 
 
-@pytest.mark.parametrize('active', 'id_person', [('random str', 1), (True, 'random str'),
-                                                 ('random str', 'random str'), (1, True)])
+@pytest.mark.parametrize('active, id_person', [('random str', 1), (True, 'random str'),
+                                               ('random str', 'random str'), (1, True)])
 def test_wrong_type(active: bool, id_person: int):
     with raises(TypeError):
         Seller(active, id_person)
